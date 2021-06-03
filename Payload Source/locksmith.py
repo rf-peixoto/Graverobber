abcf776b90efcf182d5b7a3e420f62385ac34288
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-03 18:23:45 -0300

    Update locksmith.py

--- Payload Source/locksmith.py
+++ Payload Source/locksmith.py
@@ -1,10 +1,9 @@
 from hashlib import sha256
 import sys
-
-if not sys.argv[2]:
-    print("Usage:")
-    print(sys.argv[0] + " node_id + node_sig")
-else:
+try:
     node_id = str(sys.argv[1])
     node_sig = sys.argv[2]
     print(sha256(str(node_id + "some_secret_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
+except Exception as error:
+    print("Usage:")
+    print(sys.argv[0] + " node_id + node_sig")
