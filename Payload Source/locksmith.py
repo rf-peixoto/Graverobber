64c8ba5328435cdff549fb137d158f16290b03c2
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-03 18:38:16 -0300

    Update locksmith.py

--- Payload Source/locksmith.py
+++ Payload Source/locksmith.py
@@ -3,7 +3,10 @@ import sys
 try:
     node_id = str(sys.argv[1])
     node_sig = sys.argv[2]
-    print(sha256(str(node_id + "some_secret_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
+    if int(node_id) % 2 == 0:
+        print(sha256(str(node_id + "some_secret_even_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
+    else:
+        print(sha256(str(node_id + "some_secret_odd_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
 except Exception as error:
     print("Usage:")
     print(sys.argv[0] + " node_id node_sig")
