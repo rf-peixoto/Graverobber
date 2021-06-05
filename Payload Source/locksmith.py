7ce569a78f0b5f98c6bcf85508341ba79eaf7109
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-05 11:14:07 -0300

    Update locksmith.py

--- Payload Source/locksmith.py
+++ Payload Source/locksmith.py
@@ -8,5 +8,4 @@ try:
     else:
         print(sha256(str(node_id + "some_secret_odd_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
 except Exception as error:
-    print("Usage:")
-    print(sys.argv[0] + " node_id node_sig")
+    print("Usage: " + sys.argv[0] + " [node_id] [node_sig]")
