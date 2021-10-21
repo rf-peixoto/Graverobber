8192dfac04af1f676bec5de59945754a1735732f
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-10-21 11:05:25 -0300

    Update locksmith.py
    Change SHA256 to BLAKE2S

--- Source/locksmith.py
+++ Source/locksmith.py
@@ -1,11 +1,11 @@
-from hashlib import sha256
+from hashlib import blake2s
 import sys
 try:
     node_id = str(sys.argv[1])
     node_sig = sys.argv[2]
     if int(node_id) % 2 == 0:
-        print(sha256(str(node_id + "some_secret_even_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
+        print(blake2s(str(node_id + "some_secret_even_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
     else:
-        print(sha256(str(node_id + "some_secret_odd_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
+        print(blake2s(str(node_id + "some_secret_odd_seed" + node_sig).encode()).hexdigest()[24:48][::-1])
 except Exception as error:
     print("Usage: " + sys.argv[0] + " [node_id] [node_sig]")
