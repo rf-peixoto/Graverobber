507a6815e77d8c98070d97e0f08dcff6e28c9481
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-10-29 08:13:16 -0300

    Update file_form.php

--- Server/file_form.php
+++ Server/file_form.php
@@ -3,7 +3,6 @@
   $target_dir = "uploads/";
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
   $uploadok = 1;
-
   if ($uploadok == 1) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)){
       echo "File received.";
